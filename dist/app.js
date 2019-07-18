661896d112b0e8ea2b9392e0cffe3d1904b3e598
Connor Dillon <connor@codewithconnor.com>
2019-07-18 14:39:49 -0700

    Completed UX: added load screen, show correct containers on events

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,18 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+
+    // Show loader on calculate
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 1000);
+
+    e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
     console.log('Calculating...');
 
     // UI Variables
@@ -26,17 +36,26 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // Show results
+        document.getElementById('results').style.display = 'block';
+        // Hide loading
+        document.getElementById('loading').style.display = 'none';
     } else {
         console.log('ERROR: Bad inputs');
         showError('Error: Please check your loan information and recalculate');
 
     };
 
-    e.preventDefault();
+
 }
 
 // Show Error
 function showError(error) {
+    // Show results
+    document.getElementById('results').style.display = 'none';
+    // Hide loading
+    document.getElementById('loading').style.display = 'none';
     //Create a div
     const errorDiv = document.createElement('div');
     //Get elements
